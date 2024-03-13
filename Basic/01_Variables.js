77fddcfc5a0761a7330a6df833410f53c7e59f72
Mr-Saqlain <161278525+Mr-Saqlain@users.noreply.github.com>
2024-03-13 04:14:44 +0000

    Variables

---
+++ Basic/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 765374236457657657256
+let accountEmail = "saqlain@nic.com"
+let accountPassword = "7808" // Use "let" instead of "Var".
+accountCity = "Kohat"
+let accountState; // No value means "Undeifined in JS"
+
+console.log(accountId)
+
+// accountId = 88337
+accountEmail = "munim@google.com"
+accountPassword = "7899097897"
+accountCity = "Hangu"
+
+console.table([accountId, accountEmail,accountPassword,accountCity, accountState]) 
